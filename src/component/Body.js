c17f3b3256cc5df4d55cbfb32b9fdb4987c6dd21
adeola afariogun <kokobilo55@gmail.com>
2020-05-21 18:44:37 -0700

    created Body.js to component

---
+++ src/component/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+class Body extends React.Component{
+  state = {
+    employeelist: []
+  }
+render(){
+  return (<div>
+    <h1>
+      Adeola's employee list
+    </h1>
+  </div>)
+}
+}
+
+export default Body;
