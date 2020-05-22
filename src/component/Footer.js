2c244a6d5e43a5c9c39c301f55dbdd09037df7fd
adeola afariogun <kokobilo55@gmail.com>
2020-05-21 18:37:31 -0700

    first commit

---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer(){
+  return (
+    <div className ="container">
+  <nav className ="navbar navbar-expand-lg navbar-light bg-light">
+    <a className ="navbar-brand" href="#">&copy; Adeola Afariogun, 2020</a>
+  </nav>
+  </div>)
+}
+
+
+export default Footer;
