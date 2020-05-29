286013ae8fc29ba8358b35489bd145a8877ad4f1
adeola afariogun <kokobilo55@gmail.com>
2020-05-28 19:03:03 -0700

    edited navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -11,12 +11,6 @@ function Header(){
       <li className="nav-item active">
         <a className="nav-link" href="#">React employee directory <span className="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
-      </li>
     </ul>
     <span className="navbar-text">
       Employee search using API
